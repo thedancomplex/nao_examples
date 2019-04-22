666f0b00fc30b78bb0333f43703b6f3d5d3c2919
Daniel M. Lofaro <dan@danLofaro.com>
2019-04-22 19:25:50 -0400

    added recordign with filename

--- recordAudio.sh
+++ recordAudio.sh
@@ -1,2 +1,21 @@
-NAME=$1
-arecord -D hw:1,0 -f cd -c1 $NAME.wav
+ShowUsage()
+{
+	echo
+	echo 'USB Audio Recorder For Time Synced Recordings Script'
+	echo 'Contact: Daniel M. Lofaro - dan@danLofaro.com'
+	echo 'Usage:'
+	echo '-n         - File name'
+	echo '             Example: recordAudio.sh -n robot '
+	echo '             Resulting File: robot.wav' 
+	echo
+}
+case "$1" in
+	'-n' )
+		arecord -D hw:1,0 -f cd -c1 $2.wav
+	;;
+	* )
+		ShowUsage
+		exit 1
+	;;
+esac
+exit 0
